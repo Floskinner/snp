33110785f7cd29c363c458283a5feb23c85e9cb9
Florian Glaser <58706771+Floskinner@users.noreply.github.com>
2022-04-07 15:42:24 +0200

    implemented usec and adding to list
    Co-authored-by: David <screetox@users.noreply.github.com>
    Co-authored-by: Floqueboque <Floqueboque@users.noreply.github.com>

--- pe2022/timediff.asm
+++ pe2022/timediff.asm
@@ -16,6 +16,12 @@
 ;----------------------------------------------------------------------------
 
 %include "syscall.inc"  ; OS-specific system call macros
+extern list_init
+extern list_size
+extern list_is_sorted
+extern list_add
+extern list_find
+extern list_get
 
 ;-----------------------------------------------------------------------------
 ; CONSTANTS
@@ -60,8 +66,10 @@ SECTION .text
         global _start:function  ; make label available to linker
 _start:
         nop
-        push    r12             ; save r12
-        push    r13             ; save r13
+        push    r12     ; save r12
+        push    r13     ; save r13
+
+        call    list_init       ; init list for the timeval
 
 next_string:
         ;-----------------------------------------------------------
@@ -69,7 +77,7 @@ next_string:
         ;-----------------------------------------------------------
         SYSCALL_4 SYS_READ, FD_STDIN, input_buffer, BUFFER_SIZE
         test    rax,rax         ; check system call return value
-        jz      finished        ; jump to loop exit if end of input is
+        jz      read_finished   ; jump to loop exit if end of input is
                                 ; reached, i.e. no characters have been
                                 ; read (rax == 0)
 
@@ -85,7 +93,7 @@ next_sec_char:
         xor     r8, r8                          ; clear r8
         lea     r8d, [rdx-'.']                  ; number saved in r8d
         cmp     r8b, 0                          ; check whether character is '.'
-        je      convert_sec_to_complete_number      ; yes, then read usec
+        je      convert_sec_to_complete_number  ; yes, then convert to complete number
 
 ;        char to number
         lea     r8d, [rdx-'0']  ; number saved in r8d
@@ -141,24 +149,103 @@ convert_next_sec_number:
         ; if finished continue with read_usec
 
 read_usec:
-
+        xor     rcx, rcx        ; reset counter
         cmp     dl,CHR_LF       ; check for end-of-string
         je      next_string     ; no, process next sec char in buffer
         ; check counter < 6 -> ja: next_usec, nein: next_string
 
-not_number:
+        
+
+next_usec_char:
+        movzx   edx,byte [rsi]  ; load next character from buffer
+
+        xor     r8, r8                          ; clear r8
+        lea     r8d, [rdx-CHR_LF]               ; save 0 to r8 if char is line feed
+        cmp     r8b, 0                          ; check whether character is '.'
+        je      convert_usec_to_complete_number ; yes, then convert to complete number
+
+        cmp     rcx, 0x6                        ; check whether 6 digits are read
+        jge     convert_usec_to_complete_number ; yes, then convert to complete number
+
+;       char to number
+        lea     r8d, [rdx-'0']  ; number saved in r8d
+        cmp     r8b, ('9'-'0')  ; check whether character is a number
+        ja      not_number      ; no, then end programm
+
+;       save number in r13b
+        shl     r13, 4
+        xor     r13b, r8b
+        inc     rcx             ; increment counter
+
+        inc     rsi             ; increment pointer to next char in string
+        jmp     next_usec_char  ; jump back to read next char
+
+convert_usec_to_complete_number:
+        cmp     rcx, 0x6                ; check whether 6 digits are read
+        jge     six_digits_read         ; yes, then continue with converting
+        shl     r13, 4                  ; else, add a 0 digit
+        inc     rcx                     ; ...and increment counter rcx
+        jmp     convert_usec_to_complete_number ; loop the 6 digit checker
+
+six_digits_read:
+        inc     rsi                     ; increment pointer to next char in string
+        mov     qword [usec_buffer], 0  ; clear usec_buffer
+        xor     r10, r10                ; clear r10
+        inc     r10                     ; use r10 as factor
+convert_next_usec_number:
+        xor     rdx, rdx        ; clear rdx
+        mov     dl, 0xF         ; setup rdx for the AND
+        AND     rdx, r13        ; get lowest 4 Bit from rdx
+        shr     r13, 4
+        
+        ; start number (rdx) * factor (r10)
+        push    rax             ; save rax for mul
+        mov     rax, rdx        ; move rdx to rax for mul
+        mul     r10             ; rax * 10 = edx & eax
+        shl     rdx, 32         ; to higher 32 Bit of r10
+        mov     edx, eax        ; move eax to lower 32 Bit of rdx
+        pop     rax             ; restore rax
+        ; end number (rdx) * factor (r10)
+
+        mov     r11, usec_buffer
+        add     qword [r11], rdx ; add number * factor to usec_buffer
+
+        ; start calculate factor
+        push    rax             ; save rax for mul
+        mov     rax, r10        ; move r10 to rax for mul
+        mov     r12d, 0xA       ; move factor 10 to r12
+        mul     r12d            ; rax * 10 = edx & eax
+        xor     r10, r10        ; clear r10
+        mov     r10d, edx       ; move edx...
+        shl     r10, 32         ; to higher 32 Bit of r10
+        mov     r10d, eax       ; move eax to lower 32 Bit of r10
+        pop     rax             ; restore rax
+        ; end calculate factor
+
+        dec     rcx                             ; decrement counter
+        test    rcx, rcx                        ; check if more digits available
+        jg      convert_next_usec_number        ; yes, then convert next digit
+
+        mov     r12, qword [sec_buffer]         ; add sec to timeval_buffer
+        mov     qword [timeval_buffer], r12
+        
+        mov     r12, qword [usec_buffer]        ; add usec to timeval_buffer
+        mov     qword [timeval_buffer+8], r12
+
+        mov     rdi, timeval_buffer             ; first argument of list_add need pointer to a timeval
+        call    list_add
+        jmp     next_string
+        ; if finished continue with list_add(&timeval_buffer) and next_string
+
+read_finished:
         nop
 
-finished:
-        nop
+; 1. Chcken ob liste sortiert ist
+
 
         pop     r13             ; restore r13
         pop     r12             ; restore r12
 
-; 1. Einlesen Timevals
-; <= 10 chars = sec
-;     1 char  = dot
-; <=  6 chars = usec
 
 ; 1.5                   = 0000000001.500000
 ; 1000000000.0
@@ -171,55 +258,9 @@ finished:
 ; 1502530860.999999
 ; 1502617201.999998
 
-; read sec here
-; rcd = 1 faktor
-; counter = 0
-; do {
-;        read char sec
-;        char to number
-;        save number in RAM
-;        counter++
-;
-;} while (char != ".")
 
-; rdx = 0
-; rdx => zahlenwert sec
-; convert from RAM to complete sec value
-; do {
-;         read number from RAM
-;         rdx = rdx + number from RAM * faktor
-;         faktor = faktor * 10
-;         counter--
-; } while (counter != 0)
-; 
-; save sec number
-
-; read usec here
-; rcd = 1 faktor 
-; counter = 0
-; do {
-;        read char usec
-;        char to number
-;        save number in RAM
-;        faktor = faktor * 10
-;        counter++
-;
-; } while (char != CHR_LF && counter < 6)
-;
-; rdx = zahlenwert usec
-; convert from RAM to complete usec value
-; do {
-;         read number from RAM
-;         faktor = faktor / 10
-;         rdx = rdx + number from RAM * faktor
-; } while (faktor != 1)
-
-; while (counter < 6) {
-;         rdx = rdx * 10
-; }
-
-; save timeval = [sec][usec] wenns geht
-; add timeval to list
+not_number:
+        nop
 
         ;-----------------------------------------------------------
         ; call system exit and return to operating system / shell
