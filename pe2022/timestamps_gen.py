f8eec1f7552a669384a92e5b5d131ee26b4d94df
Florian Glaser <58706771+Floskinner@users.noreply.github.com>
2022-04-11 13:56:44 +0000

    removed unessecary code

--- pe2022/timestamps_gen.py
+++ pe2022/timestamps_gen.py
@@ -4,6 +4,5 @@ import random
 start_time = time.time()
 
 for _ in range(100):
-    timestamp_str = str(start_time)
-    print(timestamp_str)
+    print(start_time)
     start_time += random.randint(0, 1000000)
